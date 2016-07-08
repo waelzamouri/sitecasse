6220592b14416f15601953886ebfc891f257b2ed
waelzamouri <waelzamouri@gmail.com>
2016-07-08 22:56:16 +0200

    Change-Id: I4ee4f72e261550d8171b6c4f9ba011488bc1bee8

---
+++ WorkspaceII/model/src/main/java/fr/wael/model/App.java
@@ -0,0 +1,13 @@
+package fr.wael.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
