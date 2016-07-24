19ae2bac55532aa8b77e6e07e923d570281dc448
waelzamouri <waelzamouri@gmail.com>
2016-07-24 22:39:32 +0200

    JQuery Module recherche

--- NetBeansWs/Presentation/Presentation/src/main/java/fr/wael/presentation/PremierBean.java
+++ NetBeansWs/Presentation/Presentation/src/main/java/fr/wael/presentation/PremierBean.java
@@ -6,7 +6,9 @@
 package fr.wael.presentation;
 
 import fr.wael.dao.imp.DaoRef_Marque;
+import fr.wael.dao.imp.DaoRef_modele;
 import fr.wael.dao.interfaces.IDaoRef_Marque;
+import fr.wael.dao.interfaces.IDaoRef_modele;
 import fr.wael.model.Ref_Marque;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
@@ -26,6 +28,7 @@ public class PremierBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private IDaoRef_Marque daoRef_Marque;
+    private IDaoRef_modele daoRef_modele;
     private Ref_Marque ref_Marque;
     private List<Ref_Marque> ref_marquess;
 
@@ -36,6 +39,7 @@ public class PremierBean implements Serializable {
         daoRef_Marque = new DaoRef_Marque();
         ref_Marque = new Ref_Marque();
         ref_marquess = new ArrayList();
+        daoRef_modele = new DaoRef_modele();
     }
 
     private List<Ref_Marque> toutesLesMarques() {
@@ -45,9 +49,9 @@ public class PremierBean implements Serializable {
         return ref_marquess;
 
     }
-    public void valueChangeMethod(ValueChangeEvent e){
-            
-	}
+//    public void valueChangeMethod(ValueChangeEvent e){
+//            
+//	}
 
     public String toRecherche() {
         return "rech";
@@ -76,5 +80,14 @@ public class PremierBean implements Serializable {
     public void setRef_marquess(ArrayList<Ref_Marque> ref_marquess) {
         this.ref_marquess = ref_marquess;
     }
+
+    public IDaoRef_modele getDaoRef_modele() {
+        return daoRef_modele;
+    }
+
+    public void setDaoRef_modele(IDaoRef_modele daoRef_modele) {
+        this.daoRef_modele = daoRef_modele;
+    }
+
 
 }
--- NetBeansWs/Presentation/Presentation/src/main/webapp/resources/js/style.js
+++ NetBeansWs/Presentation/Presentation/src/main/webapp/resources/js/style.js
@@ -3,32 +3,211 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function(){
+$(document).ready(function () {
 
-$(function(){
- 
-    $(document).on( 'scroll', function(){
- 
-    	if ($(window).scrollTop() > 100) {
-			$('.scroll-top-wrapper').addClass('show');
-		} else {
-			$('.scroll-top-wrapper').removeClass('show');
-		}
-	});
- 
-	$('.scroll-top-wrapper').on('click', scrollToTop);
+    $(function () {
+
+        $(document).on('scroll', function () {
+
+            if ($(window).scrollTop() > 100) {
+                $('.scroll-top-wrapper').addClass('show');
+            } else {
+                $('.scroll-top-wrapper').removeClass('show');
+            }
+        });
+
+        $('.scroll-top-wrapper').on('click', scrollToTop);
+    });
+
+    function scrollToTop() {
+        verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+        element = $('body');
+        offset = element.offset();
+        offsetTop = offset.top;
+        $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+    }
+
+
+
+// Autocompletion
+//Marque
+    $(function () {
+
+        var availableTags = [
+            "Renault", "Citroen", "Ford", "Peugeot", "Mercedes", "BMW",
+            "Audi", "Nissan", "Seat", "Volkswagen", "Fiat", "Alfa Rome",
+            "Hyundai", "Suzuki", "Rover", "Mitsubishi","Abarth","Ac","Acrea",
+            "Acura","Aixam","Alpina","Alpine","Amc","Aro","Asia","Aston Martin",
+            "Austin","Austin Healy","Autobianchi","Auverland","Bedford","Bellier",
+            "Bentley","Bertone","Bugati","Buick","Cadillac","Carver","Caterham","Chatenet",
+            "Chevrolet","Chrysler","Corvette","Dacia","Daewoo","Daf","Daihatsu","Daimler",
+            "Dallas","Dangel","Datsun","De la Chapelle","Delahaye","DKW/Auto Union","Dodge",
+            "Excalibur","Facel Vega","Ferrari","Ford USA","FSO","Galloper","GMC","GME","Grecav",
+            "Hommel","Honda","Infiniti","ISUZU","Iveco","Jaguar","Kia","Lancia","Land Rover",
+            "Lexus","Lotus","Mazda","MG","Mini","Porche","SAAB","Skoda","Smart","Subaru","Volvo",
+            
+        ];
+        $("#form-rech\\:marque").autocomplete({
+            source: availableTags
+        });
+    });
+
+
+
+    $(("#form-rech\\:modele")).focus(function () {
+        var modele = [];
+        //alert("fonction");
+        if ($(("#form-rech\\:marque")).val() === "Renault") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Citroen") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Peugeot") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Volkdswagen") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Ford") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Opel") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Mercedes") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Fiat") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Nissan") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Audi") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "BMW") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Toyota") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Seat") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Alfa Romeo") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Hyundai") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Suzuki") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Rover") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Mitsubishi") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Abarth") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Ac") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Acrea") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Acura") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Aixam") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Alpina") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Alpine") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "AMC") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "ARO") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Asia") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Aston Martin") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Austin Healey") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Autobianchi") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Auverland") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Bedford") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Bellier") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Bentley") {
+            modele = [
+            ];
+
+        } else if ($(("#form-rech\\:marque")).val() === "Bugatti") {
+            modele = [
+            ];
+
+        }
+        $("#form-rech\\:modele").autocomplete({
+            source: modele
+        });
+
+
+    })
+
 });
- 
-function scrollToTop() {
-	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-	element = $('body');
-	offset = element.offset();
-	offsetTop = offset.top;
-	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
-}
-
-});
-
-
-
 
