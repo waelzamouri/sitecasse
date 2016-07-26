7099f4af3944792e0f0d36ef463d232ce85e718a
waelzamouri <waelzamouri@gmail.com>
2016-07-26 19:13:24 +0200

    ajout du span à commandLink Module recherche

--- NetBeansWs/Presentation/Presentation/src/main/java/fr/wael/presentation/PremierBean.java
+++ NetBeansWs/Presentation/Presentation/src/main/java/fr/wael/presentation/PremierBean.java
@@ -9,6 +9,7 @@ import fr.wael.dao.imp.DaoRef_Marque;
 import fr.wael.dao.imp.DaoRef_modele;
 import fr.wael.dao.interfaces.IDaoRef_Marque;
 import fr.wael.dao.interfaces.IDaoRef_modele;
+import fr.wael.model.Piece;
 import fr.wael.model.Ref_Marque;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
@@ -27,67 +28,44 @@ import javax.faces.event.ValueChangeEvent;
 public class PremierBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    private IDaoRef_Marque daoRef_Marque;
-    private IDaoRef_modele daoRef_modele;
-    private Ref_Marque ref_Marque;
-    private List<Ref_Marque> ref_marquess;
 
+    private String f_marque;
+    private String f_modele;
+    private String f_nom_piece;
     /**
      * Creates a new instance of PremierBean
      */
     public PremierBean() {
-        daoRef_Marque = new DaoRef_Marque();
-        ref_Marque = new Ref_Marque();
-        ref_marquess = new ArrayList();
-        daoRef_modele = new DaoRef_modele();
+        
     }
 
-    private List<Ref_Marque> toutesLesMarques() {
-
-        ref_marquess = daoRef_Marque.getAllRef_Marque();
-        //System.out.println(ref_marquess.get(0).getNom_marque());
-        return ref_marquess;
-
+    public String getF_marque() {
+        return f_marque;
     }
-//    public void valueChangeMethod(ValueChangeEvent e){
-//            
-//	}
 
-    public String toRecherche() {
+    public void setF_marque(String f_marque) {
+        this.f_marque = f_marque;
+    }
+
+    public String getF_modele() {
+        return f_modele;
+    }
+
+    public void setF_modele(String f_modele) {
+        this.f_modele = f_modele;
+    }
+
+    public String getF_nom_piece() {
+        return f_nom_piece;
+    }
+
+    public void setF_nom_piece(String f_nom_piece) {
+        this.f_nom_piece = f_nom_piece;
+    }
+
+  
+    public String toRecherche(){
         return "rech";
     }
-
-    public IDaoRef_Marque getDaoRef_Marque() {
-        return daoRef_Marque;
-    }
-
-    public void setDaoRef_Marque(IDaoRef_Marque daoRef_Marque) {
-        this.daoRef_Marque = daoRef_Marque;
-    }
-
-    public Ref_Marque getRef_Marque() {
-        return ref_Marque;
-    }
-
-    public void setRef_Marque(Ref_Marque ref_Marque) {
-        this.ref_Marque = ref_Marque;
-    }
-
-    public List<Ref_Marque> getRef_marquess() {
-        return toutesLesMarques();
-    }
-
-    public void setRef_marquess(ArrayList<Ref_Marque> ref_marquess) {
-        this.ref_marquess = ref_marquess;
-    }
-
-    public IDaoRef_modele getDaoRef_modele() {
-        return daoRef_modele;
-    }
-
-    public void setDaoRef_modele(IDaoRef_modele daoRef_modele) {
-        this.daoRef_modele = daoRef_modele;
-    }
-
 
 }
