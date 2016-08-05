0856db0eb78be6d43e0bf1eaae3cba5aa66a9f17
waelzamouri <waelzamouri@gmail.com>
2016-08-05 19:45:15 +0200

    MàJ module marque list

--- NetBeansWs/Presentation/Presentation/src/main/java/fr/wael/presentation/BeanChargeListMarque.java
+++ NetBeansWs/Presentation/Presentation/src/main/java/fr/wael/presentation/BeanChargeListMarque.java
@@ -13,13 +13,14 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author wael
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class BeanChargeListMarque implements Serializable {
      
 
