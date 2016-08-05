0ea110a17db1465e4c26a3f4c56f7072e68066e5
waelzamouri <waelzamouri@gmail.com>
2016-08-05 21:05:13 +0200

    MàJ couleur btn

--- NetBeansWs/Presentation/Presentation/src/main/java/fr/wael/presentation/BeanChargeListMarque.java
+++ NetBeansWs/Presentation/Presentation/src/main/java/fr/wael/presentation/BeanChargeListMarque.java
@@ -13,14 +13,14 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
+
 
 /**
  *
  * @author wael
  */
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class BeanChargeListMarque implements Serializable {
      
 
